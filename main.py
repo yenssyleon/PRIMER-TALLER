44b96ee731d06b8febf377021e31278c89097a1f
yenssyleon <ykleonm@correo.udistrital.edu.co>
2022-10-22 08:56:17 -0500

    OPERACIONES v.0.0.2

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+"""
+A. La suma 
+B. El número menor 
+"""
+
+n1=int(input("Ingrese un número: "))
+n2= int(input("Ingrese otro número: "))
+
+suma=n1+n2
+
+
+if (n1<n2):
+        print("El número menor es: ",n1)
+            
+elif(n1>n2):
+        print("El número menor es: ",n2)
+    
+elif(n1==n2):
+        print("Los números son iguales")
+    
+
+print("La suma de los números es: ",suma)
+
+
+
+
+
